ebc7dedf410c57bca24e941d75ce99fbc10e7b60
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2020-09-11 23:38:54 +0200

    FizzBuzz V2

--- Main.java
+++ Main.java
@@ -4,27 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        for (int i=1; i<101; i++){
-
-            if (i%3 == 0 & i != 42 & i !=15 & i !=30 & i !=45 & i !=60 & i !=75 & i !=90){
-                System.out.println("Fizz");
-
-            }
-
-            if (i%5 == 0 & i !=15 & i !=30 & i !=45 & i !=60 & i !=75 & i !=90){
-                System.out.println("Buzz");
-            }
-
-            if (i%3 == 0 & i%5 == 0){
+        for (int i = 1; i < 101; i++) {
+            if (i == 42) {
+                System.out.println("Answer to the Ultimate");
+            } else if (i % 5 == 0 & i % 3 == 0) {
                 System.out.println("FizzBuzz");
-            }
-
-            if (i == 42){
-                System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
-            }
-
-            else if (i%3 != 0 & i%5 != 0 & i != 42){
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
                 System.out.println(i);
             }
         }
