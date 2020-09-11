413978de80b5fecd1c7e20f371bbd5b0b6f2a5b7
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2020-09-12 00:31:11 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,11 +7,11 @@ public class Main {
         for (int i = 1; i < 101; i++) {
             if (i == 42) {
                 System.out.println("Answer to the Ultimate");
-            } else if (i % 5 == 0 & i % 3 == 0) {
+            } else if (i% 5 == 0 & i% 3 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (i % 5 == 0) {
+            } else if (i% 5 == 0) {
                 System.out.println("Buzz");
-            } else if (i % 3 == 0) {
+            } else if (i% 3 == 0) {
                 System.out.println("Fizz");
             } else {
                 System.out.println(i);
