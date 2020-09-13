d57394a8028fe2f538664ebb7f373a224cf5a87b
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2020-09-13 05:12:51 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,20 +2,29 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Shayan , Islam , Leszek
 
         for (int i = 1; i < 101; i++) {
+
             if (i == 42) {
                 System.out.println("Answer to the Ultimate");
-            } else if (i% 5 == 0 & i% 3 == 0) {
+            }
+
+            else if (i% 5 == 0 & i% 3 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (i% 5 == 0) {
+            }
+
+            else if (i% 5 == 0) {
                 System.out.println("Buzz");
-            } else if (i% 3 == 0) {
+            }
+
+            else if (i% 3 == 0) {
                 System.out.println("Fizz");
-            } else {
+            }
+
+            else {
                 System.out.println(i);
             }
         }
-    } // Shayan , Islam , Leszek
+    } 
 }
